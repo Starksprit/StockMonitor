2222b1acf7a5c3f4df95e1435194f6c40fccc907
Simon Söderberg <lars.simon.soderberg@gmail.com>
2018-05-02 14:17:50 +0200

    Tried add a comment to see that it works correctly.

--- app/src/main/java/com/company/stockmonitor/MainActivity.java
+++ app/src/main/java/com/company/stockmonitor/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+//Simeon
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                     return true;
             }
             return false;
+
         }
     };
 

9e7d0770483cc6023c951f8df352398f4cbef9b2
Andreas Eliasson <svagsprit@gmail.com>
2018-05-02 14:43:50 +0200

    Added menu and simple skeleton gui

--- app/src/main/java/com/company/stockmonitor/MainActivity.java
+++ app/src/main/java/com/company/stockmonitor/MainActivity.java
@@ -11,22 +11,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_stocks:
+                    //when stocks button is pressed
+                    System.out.println("stocks");
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_search:
+                    //when search button is pressed
+                    System.out.println("search");
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_RSS:
+                    //when RSS button is pressed
+                    System.out.println("RSS");
                     return true;
             }
             return false;
@@ -39,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

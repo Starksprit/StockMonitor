da7796a10b0389a25611a54212a3180fc08fbb05
Simon Söderberg <lars.simon.soderberg@gmail.com>
2018-05-04 12:37:13 +0200

    Added a clickable listview //Simon, Emma, Alex

--- app/src/main/java/com/company/stockmonitor/MainActivity.java
+++ app/src/main/java/com/company/stockmonitor/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-//Simeon
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> stockslist = new ArrayList<>();
+
+    ListView listView;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,15 +27,17 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_stocks:
                     //when stocks button is pressed
-                    System.out.println("stocks");
+
+                    loadList();
+
                     return true;
                 case R.id.navigation_search:
                     //when search button is pressed
-                    System.out.println("search");
+
                     return true;
                 case R.id.navigation_RSS:
                     //when RSS button is pressed
-                    System.out.println("RSS");
+
                     return true;
             }
             return false;
@@ -42,6 +52,48 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        listView = (ListView) findViewById(R.id.stocksListView);
+
+        loadList();
+
+        listClickListener();
     }
+
+    public void loadList() {
+
+        stockslist.add("Stock 1");
+        stockslist.add("Stock 2");
+        stockslist.add("Stock 3");
+
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(this,
+                                        android.R.layout.simple_list_item_1, stockslist);
+
+        listView.setAdapter(stringArrayAdapter);
+
+    }
+
+    public void listClickListener() {
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (i == 0) {
+                    System.out.println("Fisk");
+
+                }
+                else if (i == 1) {
+                    System.out.println("KAtt");
+
+                }
+                else if (i == 2) {
+                    System.out.println("räkor");
+
+                }
+            }
+        });
+
+    }
+
 
 }
